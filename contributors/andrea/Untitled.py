0492240f0e09e706c401560a9187a2eb6efcace3
andrea <andrea.bruckmeier@web.de>
2024-08-21 20:41:02 +0000

    contributed a folder

---
+++ contributors/andrea/Untitled.py
@@ -0,0 +1,10 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+# ---
